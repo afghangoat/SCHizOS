da658dafb963a0ea4efb2037d7e02622433850f7
Afghan Goat <ifj.funk.gabor@gmail.com>
2025-10-05 13:40:10 +0200

    Fix commit - Mirrored from my site, file: src/kernel/task_scheduler.c

--- src/kernel/task_scheduler.c
+++ src/kernel/task_scheduler.c
@@ -4,8 +4,6 @@
 
 #define PROCESSES_MAX 32
 
-typedef bool (*Proc_func)(void);
-
 typedef struct{
 	int pid_t;
 	Proc_func p_func; //if returned false, then kill process
@@ -20,26 +18,68 @@ bool persistent_prc(){
 	return true; //Default behaviour
 }
 
-void create_process(int pid,int data){
+void create_process(int pid,int data,Proc_func func, bool persistent){
 	for(int i=0;i<jprocess;i++){
 		
 		if(global_processes[i].pid_t==-1){
 			
 			global_processes[i].pid_t=pid;
 			global_processes[i].data=data;
-			global_processes[i].p_func=persistent_prc; //currently only this is supported
-			global_processes[i].persistent=true;
+			global_processes[i].p_func=func;
+			
+			if(persistent==true){
+				
+				global_processes[i].persistent=true;
+			} else {
+				
+				global_processes[i].persistent=false;
+			}
+			
 			return;
 		}
 	}
 	
 	global_processes[jprocess].pid_t=pid;
 	global_processes[jprocess].data=data;
-	global_processes[jprocess].p_func=persistent_prc; //currently only this is supported
-	global_processes[jprocess].persistent=true;
+	global_processes[jprocess].p_func=func;
+	if(persistent==true){
+		global_processes[jprocess].persistent=true;
+	} else {
+		
+		global_processes[jprocess].persistent=false;
+	}
 	
 	jprocess++;
 }
+
+/*void create_process(int pid, int data, ) {
+    // If no function is provided, use the default persistent_prc
+    if (func == NULL) {
+        func = persistent_prc;
+    }
+
+    // Look for a free slot first
+    for (int i = 0; i < jprocess; i++) {
+        if (global_processes[i].pid_t == -1) {
+            global_processes[i].pid_t = pid;
+            global_processes[i].data = data;
+            global_processes[i].p_func = func;
+            global_processes[i].persistent = persistent;
+            return;
+        }
+    }
+
+    // No free slot found, append at the end if there is room
+    if (jprocess < PROCESSES_MAX) {
+        global_processes[jprocess].pid_t = pid;
+        global_processes[jprocess].data = data;
+        global_processes[jprocess].p_func = func;
+        global_processes[jprocess].persistent = persistent;
+        jprocess++;
+    } else {
+        printf("Error: Max processes reached, cannot create process %d\n", pid);
+    }
+}*/
 
 bool kill_process(int pid){
 	bool killed=false;
@@ -81,3 +121,16 @@ void init_task_scheduler(){
 		global_processes[i].pid_t=-1; //clear
 	}
 }
+
+void run_scheduler_loop() {
+    for (int i = 0; i < jprocess; i++) {
+        if (global_processes[i].pid_t != -1) {
+            bool keep_running = global_processes[i].p_func();
+            if (!keep_running) {
+                global_processes[i].pid_t = -1;
+                printf("Process %d was finished.\n", i);
+            }
+        }
+    }
+}
+
