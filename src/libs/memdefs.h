7b0454f5e3f704e00e3b97da94911950e137d6b6
Afghan Goat <ifj.funk.gabor@gmail.com>
2025-10-05 13:40:30 +0200

    Fix commit - Mirrored from my site, file: src/libs/memdefs.h

--- src/libs/memdefs.h
+++ src/libs/memdefs.h
@@ -4,13 +4,13 @@
 //0x00000400 - 0x000004FF - BIOS data area
 
 #define MEMORY_MIN 0x00000500
-#define MEMORY_MAX 0x00080000
+#define MEMORY_MAX 0x00090000
 
 //0x00000500 - 0x00010500 - FAT driver
 //#define MEMORY_FAT_ADDR ((void far*)0x00500000) //segment offset
 //#define MEMORY_FAT_MAX 0x00010000
-#define MEMORY_FAT_ADDR     ((void*)0x20000)
-#define MEMORY_FAT_MAX     0x00010000
+#define MEMORY_FAT_ADDR     ((void*)0x70000)
+#define MEMORY_FAT_MAX     0x00020000
 
 #define MEMORY_LOAD_KERNEL  ((void*)0x30000)
 #define MEMORY_LOAD_SIZE    0x00010000
