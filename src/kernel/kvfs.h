1bc5bf8380a34a2228bc1aba5ca3b02829e4c70b
Funk Gábor <91252764+afghangoat@users.noreply.github.com>
2025-11-01 16:32:12 +0100

    Fix commit FS - Mirrored from my site, file: src/kernel/kvfs.h

--- src/kernel/kvfs.h
+++ src/kernel/kvfs.h
@@ -7,8 +7,12 @@
 #include "memdefs.h"
 #include "memory.h"
 
+#define ROOT_SYMBOL "#"
+#define ROOT_SYMBOL_CHAR '#'
+
 static MemoryFileEntry GlobalEntries[MAX_FILE_ENTRIES_IN_LIVE_BOOT];
 static int LoadedFileSizes=0;
+static char CurrentPath[MAX_DIR_NAME_DEPTH]=ROOT_SYMBOL;
 
 extern char* initrd;
 extern uint32_t initrd_size;
@@ -18,6 +22,13 @@ extern uint32_t initrd_size;
 //int a();
 
 void printRoot();
+
+void printDir();
+
+void setCurrentPath(const char* newpath);
+void setPathToRoot();
+
+void pwd();
 
 bool readFromBoot(MemoryFileEntry entries[],uint32_t file_count);
 
