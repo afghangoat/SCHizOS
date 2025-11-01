08aa26eb574778b7aa0aeca679ace3f927989dd7
Funk Gábor <91252764+afghangoat@users.noreply.github.com>
2025-11-01 16:32:41 +0100

    Fix commit FS - Mirrored from my site, file: src/libs/fs/vfs.h

--- src/libs/fs/vfs.h
+++ src/libs/fs/vfs.h
@@ -4,6 +4,8 @@
 
 #define MAX_FILE_SIZE_IN_LIVE_BOOT 512
 
+#define MAX_DIR_NAME_DEPTH 255
+
 #ifndef FAT_NAME_LENGTH
 	#define FAT_NAME_LENGTH 11
 #endif
@@ -11,6 +13,7 @@
 typedef struct {
 	char FileContents[MAX_FILE_SIZE_IN_LIVE_BOOT];
 	char FileName[FAT_NAME_LENGTH];
+	char InDir[MAX_DIR_NAME_DEPTH];
 } MemoryFileEntry;
 
 MemoryFileEntry copyToFile(const char* data);
